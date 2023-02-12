9ae5b34f9190f9b5110cb01ba24be30cdf43c7da
Alex <alex_russ@bk.ru>
2023-02-12 07:14:59 +0500

    Создан генератор списка бочонков мешка, он в init, затем в  функции take barrel   из мешка берем последний - new_barrel = self.new_barrels.pop()

--- pouch.py
+++ pouch.py
@@ -1,8 +1,6 @@
 import games
 import random
-
 """модуль с классом Мешок, а в нем бочонки"""
-
 
 class Pouch:
     """Мешок с бочонками"""
@@ -11,7 +9,9 @@ class Pouch:
         self.remains = remains  # осталось в мешке, от 90 за  ход  уменьшается на 1
         self.taken = taken  # использованны, в начале  игры 0
         self.taken_barrels = []  # список извлеченных бочонков
-        self.new_barrel = None #извлеченный бочонок
+        new_barrels = [i for i in range(1, 91)]
+        self.new_barrels = random.sample(new_barrels, len(new_barrels))
+
     def __str__(self):
         rep = f'Всего в мешке бочонков: {self.remains}, извлекли: {self.taken}, \n ' \
               f'список извлеченных: {self.taken_barrels}'
@@ -28,16 +28,17 @@ class Pouch:
         """достать ОДИН бочонок из мешка, - 1 ход"""
         #take = games.ask_yes_no(question="Достать бочонок(y/n)?: ")
         #if (take == "y" or take == "н"):
-        self.new_barrel = random.randint(1, 90)  # случайный номер от 1 до 90
-        if self.new_barrel not in self.taken_barrels:
-            self.taken_barrels.append(self.new_barrel)
+        new_barrel = self.new_barrels.pop()
+
+        #self.new_barrel = random.randint(1, 90)  # случайный номер от 1 до 90
+        if new_barrel not in self.taken_barrels:
+            self.taken_barrels.append(new_barrel)
             self.remains -= 1  # уменьшает количество бочонков  в мешке на 1
             self.taken += 1  # увеличивает количество бочонков  на  столе на 1
-            print(f'\nВыпал Бочонок с номером:  *** {self.new_barrel} ***, осталось ходов: {self.remains}')
-            return self.new_barrel
+            print(f'\nВыпал Бочонок с номером:  *** {new_barrel} ***, осталось ходов: {self.remains}')
+            return new_barrel
 
 
 if __name__ == "__main__":
     input("\n\nНажмите  Enter, чтобы выйти.")
-
 
