abe483449bcb2ad00c28306cc2d838a5282b8ccd
Alex <alex_russ@bk.ru>
2023-02-26 08:46:44 +0500

    удалены дишние строки

--- pouch.py
+++ pouch.py
@@ -6,8 +6,6 @@ class Pouch:
     """Мешок с бочонками"""
 
     def __init__(self):  # инициализация объекта  класса
-        # self.remains = remains  # осталось в мешке, от 90 за  ход  уменьшается на 1
-        # self.taken = taken  # использованны, в начале  игры 0
         self.all_barrels = random.sample([i for i in range(1, 91)], 90)
         self.taken_barrels = []  # список извлеченных бочонков
 
@@ -26,25 +24,10 @@ class Pouch:
         #if (take == "y" or take == "н"):
         self.all_barrels = random.sample(self.all_barrels, len(self))#встряхнуть мешок
         new_barrel = self.all_barrels.pop()
-
-        #self.new_barrel = random.randint(1, 90)  # случайный номер от 1 до 90
-        if new_barrel not in self.taken_barrels:
-            self.taken_barrels.append(new_barrel)
-            # self.remains -= 1  # уменьшает количество бочонков  в мешке на 1
-            # self.taken += 1  # увеличивает количество бочонков  на  столе на 1
-            print(f'\nВыпал Бочонок с номером:  *** {new_barrel} ***, осталось ходов:  {len(self)}')
-            return new_barrel
-
-
+        self.taken_barrels.append(new_barrel)
+        print(f'\nВыпал Бочонок с номером:  *** {new_barrel} ***, осталось ходов:  {len(self)}')
+        return new_barrel
 
 # if __name__ == "__main__":
 #     input("\n\nНажмите  Enter, чтобы выйти.")
 
-pouch = Pouch()
-print(pouch)
-print(pouch.all_barrels)
-print(len(pouch))
-pouch.take_barrel()
-print(len(pouch))
-print(pouch.all_barrels)
-print(pouch)
